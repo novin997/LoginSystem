baf5acb9a6bec270ed1ead0097e63e7a7f377c0e
novin997 <cocolupiayi@gmail.com>
2020-11-21 23:20:00 +0800

    Add react router for different pages

--- front/src/Anime.tsx
+++ front/src/Anime.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Icon } from "semantic-ui-react";
 import "./Anime.scss";
 
-export default function Anime() {
+export default function Anime({ toggle }: { toggle: () => void }) {
   return (
     <div>
       <div className="sidebar-icon">
-        <Icon circular name="sidebar" size="large" />
+        <Icon circular name="sidebar" size="large" onClick={toggle} />
       </div>
+      <h1>Anime</h1>
     </div>
   );
 }
--- front/src/SideBar.tsx
+++ front/src/SideBar.tsx
@@ -1,45 +1,92 @@
-import React, { useState } from "react";
+import React, { createContext, useState } from "react";
+import { BrowserRouter, Link, Redirect, Route, Switch } from "react-router-dom";
 import { Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
 import Anime from "./Anime";
+import Login from "./Login";
 import "./SideBar.scss";
 
-export default function SideBar() {
-  const [collapsed, setCollapsed] = useState(true);
+// Interface for AuthContext
+interface AuthInterface {
+  isAuth: boolean;
+  setAuth: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-  function toggle() {
-    setCollapsed(!collapsed);
+//Create AuthContext so that we can keep track if people is login or not
+export const AuthContext = createContext<AuthInterface | undefined>(undefined);
+
+export default function SideBar() {
+  const [isCollapsed, setCollapsed] = useState(true);
+  const [isAuth, setAuth] = useState(false);
+
+  function toggle(): void {
+    setCollapsed(!isCollapsed);
   }
 
   return (
-    <div className="sidebar">
-      <Sidebar.Pushable as={Segment}>
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          icon="labeled"
-          inverted
-          vertical
-          visible={collapsed}
-          width="thin"
-        >
-          <Menu.Item as="a">
-            <Icon name="home" />
-            Home
-          </Menu.Item>
-          <Menu.Item as="a">
-            <Icon name="gamepad" />
-            Games
-          </Menu.Item>
-          <Menu.Item as="a">
-            <Icon name="camera" />
-            Channels
-          </Menu.Item>
-        </Sidebar>
+    <AuthContext.Provider value={{ isAuth, setAuth }}>
+      <BrowserRouter>
+        <div className="sidebar">
+          <Sidebar.Pushable as={Segment}>
+            <Sidebar
+              as={Menu}
+              animation="overlay"
+              icon="labeled"
+              inverted
+              vertical
+              visible={!isCollapsed}
+              width="thin"
+            >
+              <Link to="/">
+                <Menu.Item as="a">
+                  <Icon name="home" />
+                  Home
+                </Menu.Item>
+              </Link>
 
-        <Sidebar.Pusher>
-          <Anime />
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
-    </div>
+              <Link to="/anime">
+                <Menu.Item as="a">
+                  <Icon name="gamepad" />
+                  Anime
+                </Menu.Item>
+              </Link>
+
+              <Link to="/games">
+                <Menu.Item as="a">
+                  <Icon name="camera" />
+                  Games
+                </Menu.Item>
+              </Link>
+            </Sidebar>
+
+            <Switch>
+              <Route exact path="/">
+                <Sidebar.Pusher>
+                  <Anime toggle={toggle} />
+                </Sidebar.Pusher>
+              </Route>
+              <Route exact path="/anime">
+                <Sidebar.Pusher>
+                  {isAuth ? (
+                    <Anime toggle={toggle} />
+                  ) : (
+                    <Redirect to="/login" />
+                  )}
+                </Sidebar.Pusher>
+              </Route>
+              <Route exact path="/games">
+                <Sidebar.Pusher>
+                  <Anime toggle={toggle} />
+                </Sidebar.Pusher>
+              </Route>
+              <Route exact path="/login">
+                <Sidebar.Pusher>
+                  <Login />
+                </Sidebar.Pusher>
+              </Route>
+            </Switch>
+          </Sidebar.Pushable>
+        </div>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
