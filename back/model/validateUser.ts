17420cb73a5557c644210aa620c9bde0f6628667
novin997 <cocolupiayi@gmail.com>
2020-11-28 19:24:59 +0800

    enhance the adduser controller with validation

---
+++ back/model/validateUser.ts
@@ -0,0 +1,19 @@
+import Joi from "@hapi/joi";
+
+interface schemaInterface {
+  username: string;
+  email: string;
+  password: string;
+}
+
+const schema: Joi.ObjectSchema<schemaInterface> = Joi.object({
+  username: Joi.string().min(6).max(255).required(),
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(6).max(1024).required(),
+});
+
+async function validateUser(body: Body): Promise<Joi.ValidationResult> {
+  return schema.validate(body);
+}
+
+export = validateUser;
--- back/routes/auth.ts
+++ back/routes/auth.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import User from "../model/user";
+import validateUser from "../model/validateUser";
 
 const router = express.Router();
 
 router.post("/adduser", async (req, res) => {
+  const validation = await validateUser(req.body);
+  if (validation.error) {
+    return res.status(400).json({ error: validation.error.details[0].message });
+  }
+  const isEmailExist = await User.findOne({ email: req.body.email });
+  if (isEmailExist) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+
   const user = new User({
     username: req.body.username,
     email: req.body.email,
